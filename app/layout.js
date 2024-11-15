be4ee3d36c6f867f67b79afe5d0026f0d0a5140a
Tepag <z190tpg@gmail.com>
2024-11-15 15:40:34 +0100

    aggiunta navbar

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import localFont from "next/font/local";
+import Navbar from "./ui/navbar";
+
 import "./globals.css";
 
 const geistSans = localFont({
@@ -20,9 +22,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
+        <Navbar />
         {children}
       </body>
     </html>
