94ee7618c798c3c76f95b796ad829d45ce103ec8
Tepag <z190tpg@gmail.com>
2024-11-29 22:34:31 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,42 @@
-export default function Home() {
+// "use client"
+// import { motion } from "framer-motion"
 
+// const aniamtion1 = {
+//     hidden: {
+//       scale: .8,
+//       opacity: 0
+//     },
+//     visible: {
+//       scale: 1,
+//       opacity: 1,
+//       transition: {
+//         delay: .4
+//       }
+//     },
+// }
+
+// export default function Template({ children }: { children: React.ReactNode }) {
+//   return (
+
+//     <motion.div initial="hidden" animate="visible" variants={aniamtion1}>
+//       <h1 className="title">
+//         Wubba Lubba Dub Dub!
+//       </h1>
+//       {children}
+//     </motion.div>
+    
+//   )
+// }
+
+"use client"
+
+
+export default function Home() {
+  
   return (
+    
     <>
+  
       <div className="box" style={{ opacity: 0 }}></div>
         <div className="relative px-6 pt-14 lg:px-8">
           
