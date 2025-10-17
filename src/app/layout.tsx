98037f59ed159e02c7cda5e8a387fbb6f0b2f676
Tepag <z190tpg@gmail.com>
2025-10-17 20:26:33 +0200

    Fix I18nProvider import - use named export instead of default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
-import I18nProvider from "./components/I18nProvider";
+import { I18nProvider } from "./components/I18nProvider";
 import Script from "next/script";
 
 const geistSans = Geist({
