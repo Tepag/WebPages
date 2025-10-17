b384c7db46bbe9de9406334dad00d671c0fa8ec2
Tepag <z190tpg@gmail.com>
2025-10-17 19:18:18 +0200

    Separate CSS files for local vs GitHub Pages: clean solution for different environments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
-import CSSPathFixer from "./components/CSSPathFixer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,19 +32,29 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=LXGW+WenKai+TC&display=swap" rel="stylesheet" />
         <link rel="icon" href="/assets/images/logo.png" />
-        <link rel="stylesheet" href="/css/main.css" />
+        <link rel="stylesheet" href="/css/main-local.css" id="main-css" />
         <link rel="stylesheet" href="/css/vendor/introgridsection.css" />
         <link rel="stylesheet" href="/css/vendor/joinUs.css" />
         <link rel="stylesheet" href="/css/vendor/purecssflipcard.css" />
         <link rel="stylesheet" href="/css/vendor/event.css" />
       </head>
       <body className="lxgw-wenkai-tc-regular" style={{ backgroundColor: 'black' }}>
-        <CSSPathFixer />
         {children}
         <Script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js" strategy="beforeInteractive" />
         <Script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/Flip.min.js" strategy="beforeInteractive" />
         <Script src="https://cdn.jsdelivr.net/gh/studio-freight/lenis@1.0.29/bundled/lenis.min.js" strategy="beforeInteractive" />
         <Script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js" strategy="afterInteractive" />
+        <Script id="css-switcher" strategy="beforeInteractive">
+          {`
+            (function() {
+              const isGitHubPages = window.location.hostname.includes('github.io');
+              const cssLink = document.getElementById('main-css');
+              if (isGitHubPages && cssLink) {
+                cssLink.href = '/css/main-github.css';
+              }
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
